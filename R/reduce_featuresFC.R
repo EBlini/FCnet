2a7fc1f45f9e4d74589e69057d4013bb8864018f
EBlini <57792743+EBlini@users.noreply.github.com>
2020-11-25 14:45:56 +0100

    reduce_featuresFC function added
    reduce_featuresFC function added

---
+++ R/reduce_featuresFC.R
@@ -0,0 +1,282 @@
+
+#' Reduce the dimensionality of Functional Connectivity matrices
+#'
+#' This function accepts a list of lists containing square FC matrices
+#' - such as one created by `loadFC()` - and apply feature reduction techniques.
+#' Available techniques are listed under the `method` parameter and currently
+#' include Principal Component Analysis (PCA) and Independent Component
+#' Analysis (ICA). Both are applied on the upper triangular part of the matrix.
+#' PCA uses base R `prcomp()` while ICA uses `ica::icafast()`,
+#' thus the `ica` package must be installed. The user can supply the desired
+#' number of components to retain; else, the number of components needed to
+#' explain at least a given proportion of the variance of FC matrices will be returned under
+#' the `Weights` slot - for ICA, this is based on the PCA analysis.
+
+
+#' @param FCmatrices a list of lists including the (square) FC matrices, such as
+#' one provided by `loadFC()`.
+#' @param method One of "PCA" or "ICA", defining the method for the desired
+#' feature reduction technique.
+#' @param Ncomp Number of components to retain. The default (NULL) automatically
+#' retains 95% of the explained variance. If Ncomp== "all" returns all
+#' the components. If Ncomp <1 this is interpreted as if the user wishes to retain
+#' a given proportion of variance (e.g. 0.6). If Ncomp is a vector, an additional
+#' slot is returned: a list of lists offering solutions with many components
+#' (slightly redundant for PCA but possibly useful for crossvalidating the number
+#' of features). In that last case the Weight slot always returns the model
+#' accounting for at least 95% of variance.
+#' @param parallel If TRUE uses `future.apply::future_lapply()`:
+#' `future.apply` must be installed, your machine should have multiple cores
+#' available for use, and threads should be defined explicitly
+#' beforehand by the user (e.g. by calling `plan(multisession)`). Provided
+#' these conditions are met, this can sensibly speed up the process - especially
+#' for ICA estimations - when solutions with different components are asked
+#' (i.e. Ncomp is a vector).
+#' @param explore If TRUE only returns the proportion of explained variance
+#' of all components, possibly useful to inform analyses.
+#' @param ... More commands passed to `ica::icafast()`.
+
+#' @return Weights is a matrix of reduced features. Loadings contains either the
+#' eigenvectors or the estimated sources mapping the reduced features onto the
+#' original space. SummaryPCA reports the amount of explained variance of the features
+#' with respect to variability in the original matrices. MeanFC is the mean
+#' FC matrix. CrossWeights (optional) is a list of lists reporting weights and
+#' loadings for a range of provided Ncomp.
+
+#' @export
+
+
+reduce_featuresFC= function(FCmatrices,
+                            method= c("PCA", "ICA"),
+                            Ncomp= NULL,
+                            parallel= F,
+                            explore= F,
+                            ...){
+
+  method= match.arg(method)
+
+  #is Ncomp a vector?
+  if(! is.null(Ncomp) & length(Ncomp)>1){
+    Ncomp_vector= Ncomp
+    Ncomp= NULL
+  } else {
+
+    Ncomp_vector= NULL
+
+  }
+
+  #First collapse the upper triangles
+  if(parallel){
+
+    FC= future.apply::future_lapply(FCmatrices, function(x){
+
+      x[upper.tri(x, diag = F)]
+
+    }, future.seed = T)
+
+  } else {
+
+    FC= lapply(FCmatrices, function(x){
+
+      x[upper.tri(x, diag = F)]
+
+    })
+
+  }
+
+  #then merge
+  FC= do.call(rbind, FC)
+
+
+  #first PCA - which is needed anyway
+  PCA= prcomp(FC)
+
+  summaryPCA= summary(PCA)$importance
+
+  Ncomp95= as.numeric(which(summaryPCA[3,]>0.95)[1])
+
+  if(explore){
+
+    print(paste("Components needed to explain 95% of the variance:",
+                Ncomp95))
+
+    return(summaryPCA)
+
+  } else {
+
+    #Ncomp changes accordingly
+    if(is.null(Ncomp)){
+
+      Ncomp= Ncomp95
+
+    }
+
+    if(Ncomp== "all"){
+
+      Ncomp= length(summaryPCA[3,])
+
+    }
+
+    if (Ncomp<1){
+
+      Ncomp= as.numeric(which(summaryPCA[3,]>Ncomp)[1])
+
+    }
+
+    if (Ncomp>length(summaryPCA[3,])){
+
+      warning("You asked for too many components!")
+
+    }
+
+    if(method== "PCA"){
+
+      #you already have the PCA object
+      Loadings= PCA$rotation[,1:Ncomp]
+      Weights= predict(PCA,
+                       newdata= FC)[,1:Ncomp]
+
+      if(!is.null(Ncomp_vector)){
+
+        if(parallel){
+
+          CrossWeights= future.apply::future_lapply(Ncomp_vector, function(x){
+
+            L= PCA$rotation[,1:x]
+            W= predict(PCA,
+                       newdata= FC)[,1:x]
+            return(list(Weights= W,
+                        Loadings= L))
+
+          }, future.seed = T)
+
+        } else {
+
+          CrossWeights= lapply(Ncomp_vector, function(x){
+
+            L= PCA$rotation[,1:x]
+            W= predict(PCA,
+                       newdata= FC)[,1:x]
+
+            return(list(Weights= W,
+                        Loadings= L))
+
+        })
+
+        }
+
+        names(CrossWeights)= paste0("PCA_", Ncomp_vector)
+
+      } #end if vector
+
+    } else {
+
+      #method: "ICA"
+      #preprocess
+      ppFC= t(FC)
+      ppFC= ppFC - rowMeans(ppFC)
+
+      #if a vector was provided
+      if(!is.null(Ncomp_vector)){
+
+        #get CrossWeights
+        if(parallel){
+
+          CrossWeights= future.apply::future_lapply(Ncomp_vector, function(x){
+
+            res= icafast(ppFC, nc = x, ...)
+
+            L= res$S
+            W= res$M
+
+            return(list(Weights= W,
+                        Loadings= L))
+
+          }, future.seed = T)
+
+        } else {
+
+          CrossWeights= lapply(Ncomp_vector, function(x){
+
+            res= icafast(ppFC, nc = x, ...)
+
+            L= res$S
+            W= res$M
+
+            return(list(Weights= W,
+                        Loadings= L))
+
+          })
+
+
+        } #end if parallel
+
+        #now, tricky to match Ncomp: is it within the original vector?
+        if(Ncomp %in% Ncomp_vector){
+          Weights= CrossWeights[[which(Ncomp_vector== Ncomp)]]$Weights
+          Loadings= CrossWeights[[which(Ncomp_vector== Ncomp)]]$Loadings
+        } else {
+
+          Weights= NULL
+          Loadings=NULL
+
+        }
+
+        names(CrossWeights)= paste0("ICA_", Ncomp_vector)
+
+      } # end if vector
+
+      if(is.null(Ncomp_vector) | !Ncomp %in% Ncomp_vector){
+
+        #do it
+        ICA= icafast(ppFC, nc = Ncomp, ...)
+
+        Loadings= ICA$S
+        Weights= ICA$M
+
+      }
+
+
+    } #end method
+
+    #merge and return
+    res= list(Weights= Weights,
+              Loadings= Loadings,
+              summaryPCA= summaryPCA)
+
+    #also return mean FC matrix
+    if(parallel){
+
+      meanFC= Reduce("+",
+                     future.apply::future_lapply(FCmatrices, function(x) {
+
+                       as.matrix(x)/ length(FCmatrices)
+
+                     }, future.seed = T))
+
+    } else {
+
+      meanFC= Reduce("+",
+                   lapply(FCmatrices, function(x) {
+
+                     as.matrix(x)/ length(FCmatrices)
+
+                     }))
+    }
+
+    res[["MeanFC"]]= meanFC
+
+    #merge crossweights if vector was provided
+
+    if(!is.null(Ncomp_vector)){
+
+      res[["CrossWeights"]]= CrossWeights
+
+    }
+
+    #return list (of lists)
+    return(res)
+
+  } #end if explore
+
+}
