30966350d175bbe2a6b2cb09d97870aa51439ad9
EBlini <57792743+EBlini@users.noreply.github.com>
2021-03-24 16:05:53 +0100

    documented

--- R/permutateLOO.R
+++ R/permutateLOO.R
@@ -25,9 +25,12 @@
 #' is optimized through internal crossvalidation. It defaults to a vector
 #' ranging from 10^-5 to 10^5 with 200 values in logarithmic steps.
 #' @param cv_Ncomp Whether to crossvalidate the number of components or not.
-#' It defaults to NULL, but a vector can be supplied specifing the number of
+#' It defaults to NULL, but a vector can be supplied specifing the number (range) of
 #' components to test in the inner loops.
-
+#' @param cv_Ncomp_method Whether the number of components to optimize means
+#' components are ordered (e.g. according to the explained variance of neuroimaging
+#' data) or - somehow experimental - whether to use the N best components
+#' ranked according to their relationship (MSE) with y.
 #' @param parallelLOO If TRUE - recommended, but not the default - uses
 #' `future.apply::future_lapply()` for the outer loops: `future.apply` must be
 #' installed, the machine should have multiple cores available for use,
@@ -67,6 +70,7 @@ permutateLOO= function(y,
                        alpha= seq(0, 1, by= 0.1),
                        lambda= rev(10^seq(-5, 5, length.out = 200)),
                        cv_Ncomp= NULL,
+                       cv_Ncomp_method= c("order", "optimized"),
                        parallelLOO= F,
                        scale_y= T,
                        scale_x= T,
@@ -77,6 +81,8 @@ permutateLOO= function(y,
                        intercept= optionsFCnet("intercept"),
                        standardize= optionsFCnet("standardize"),
                        ...){
+
+  cv_Ncomp_method= match.arg(cv_Ncomp_method)
 
   #ensure you are working with matrices
   y= data.matrix(y)
@@ -107,6 +113,7 @@ permutateLOO= function(y,
                           alpha = alpha,
                           lambda= lambda,
                           cv_Ncomp = cv_Ncomp,
+                          cv_Ncomp_method = cv_Ncomp_method,
                           parallelLOO= parallelLOO,
                           scale_y= F,
                           scale_x= F,
@@ -135,6 +142,7 @@ permutateLOO= function(y,
                     alpha = alpha,
                     lambda= lambda,
                     cv_Ncomp = cv_Ncomp,
+                    cv_Ncomp_method = cv_Ncomp_method,
                     parallelLOO= F,
                     scale_y= F,
                     scale_x= F,
@@ -169,6 +177,7 @@ permutateLOO= function(y,
                       alpha = alpha,
                       lambda= lambda,
                       cv_Ncomp = cv_Ncomp,
+                      cv_Ncomp_method = cv_Ncomp_method,
                       parallelLOO= F,
                       scale_y= F,
                       scale_x= F,
